9c9e801917f7ef447e5bdaa95e8e5337577e2f7b
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-02-18 13:13:19 +0200

    fix(navigation): Update navigation bar - Update js for the color change and class updates

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -9,22 +9,4 @@ export function initNavHighlight() {
         e.target.classList.add('active');
       });      
     });
-  
-    // Update active link on scroll
-    /*window.addEventListener('scroll', () => {
-      const sections = document.querySelectorAll('section');
-      
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        if (window.scrollY >= (sectionTop - 100)) {
-          const id = section.getAttribute('id');
-          navLinks.forEach(link => {
-            link.classList.remove('active');
-            if (link.getAttribute('data-section') === id) {
-              link.classList.add('active');
-            }
-          });
-        }
-      });
-    });*/
   }
