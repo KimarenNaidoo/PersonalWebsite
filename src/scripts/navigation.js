dc03d0b125b5c2b95fd3e1e387b5873d5be70ff2
KimarenNaidoo <kimarennaidoo@gmail.com>
2025-03-04 13:15:03 +0200

    feat:Update headings to change color when active.

--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -2,11 +2,21 @@
 export function initNavHighlight() {
     const navLinks = document.querySelectorAll('.nav-link');
     if (!navLinks.length) return;
+
+	const titles = document.querySelectorAll('.title');
+    if (!titles.length) return;
   
     navLinks.forEach(link => {
       link.addEventListener('click', (e) => {
         navLinks.forEach(link => link.classList.remove('active'));
+		titles.forEach(title => title.classList.remove('active'));
+
         e.target.classList.add('active');
-      });      
+		
+		titles.forEach(title => {
+			if (link.innerText == title.innerText)
+				title.classList.add("active");
+		});
+      });    
     });
-  }
+}
